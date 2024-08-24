efabb9319cb6806a33689ab99a5917e163187db2
Mahbubul Hasan <mahbubul161@gmail.com>
2024-08-24 08:11:09 +0600

    Config laravel permission package

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,13 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles;
 
     /**
      * The attributes that are mass assignable.
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class RoleSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+
+        $user = User::find(1);
+
+        Role::insert(
+            [
+                ['name' => 'Admin'],
+                ['name' => 'User'],
+            ]
+        );
+
+        $role = Role::find(1);
+        $user->assignRole($role);
+
+        $permissions = [
+            [
+                'group_name'  => 'dashboard',
+                'permissions' => [
+                    'Dashboard',
+                ],
+            ],
+            [
+                'group_name'  => 'Url',
+                'permissions' => [
+                    'Short url list',
+                ],
+            ],
+            [
+                'group_name'  => 'User',
+                'permissions' => [
+                    'User list',
+                ],
+            ],
+            [
+                'group_name'  => 'Settings',
+                'permissions' => [
+                    'Show settings',
+                    'Edit category',
+                ],
+            ],
+            [
+                'group_name'  => 'Permission',
+                'permissions' => [
+                    'Permission list',
+                    'Create permission',
+                    'Edit permission',
+                    'Delete permission',
+                ],
+            ],
+            [
+                'group_name'  => 'Role',
+                'permissions' => [
+                    'Role list',
+                    'Create role',
+                    'Edit role',
+                    'Delete role',
+                ],
+            ],
+        ];
+
+        for ($i = 0; $i < count($permissions); $i++) {
+            for ($j = 0; $j < count($permissions[$i]['permissions']); $j++) {
+                $permission = Permission::create([
+                    'name'       => $permissions[$i]['permissions'][$j],
+                    'group_name' => $permissions[$i]['group_name'],
+                ]);
+                $role->givePermissionTo($permission);
+            }
+        }
+    }
+}
